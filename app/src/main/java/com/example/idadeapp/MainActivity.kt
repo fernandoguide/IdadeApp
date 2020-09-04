5727c0d083473bc623bc905c41681c91a7c094a5
fernandoguide <fernando-guide@hotmail.com>
2020-09-04 16:49:15 -0300

    criando as validadcoes do app

--- app/src/main/java/com/example/idadeapp/MainActivity.kt
+++ app/src/main/java/com/example/idadeapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.idadeapp
 
-import android.content.res.ColorStateList
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +7,7 @@ import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,35 +17,59 @@ class MainActivity : AppCompatActivity() {
 
     fun enviar(view: View) {
         txv_Resposta.setTextColor(Color.BLACK)
-        val anoNasicmento = Calendar.getInstance().get(Calendar.YEAR)
-        val resposta = anoNasicmento - etn_Idade.text.toString().toInt()
-        val nome = ttp_Nome.text.toString()
-        val sobreNome = ttp_Sobrenome.text.toString()
-        val ano = etn_Idade.text.toString()
-        validarCampos(nome , sobreNome , ano )
+        val anoAtual = Calendar.getInstance().get(Calendar.YEAR)
+        val anoNasc = etn_Idade.text.toString()
+        if(anoNasc != ""){
+            val resposta = anoAtual - anoNasc.toInt()
+            val nome = ttp_Nome.text.toString().trim()
+            val sobreNome = ttp_Sobrenome.text.toString()
+            val ano = etn_Idade.text.toString()
+            val camposValidos = validarCampos(nome,sobreNome,ano)
+            if(camposValidos)
+                respostaIdade(resposta.toString(),nome,sobreNome)
+        }else {
+            Toast.makeText(this, "O ano deve conter 4 digitos", Toast.LENGTH_SHORT).show()
+        }
 
-        respostaIdade(resposta)
+
+
     }
 
-    private fun respostaIdade(resposta: Int) {
-        if (resposta > 18) {
-            txv_Resposta.text = "A sua idade é : ${resposta} anos! Já pode dirigir!!"
-            txv_Resposta.setTextColor(Color.RED)
-        } else if (resposta < 18) {
-            txv_Resposta.text = "A sua idade é : ${resposta} anos! Você ainda é menor de idade!"
+    private fun respostaIdade(resposta: String,nome:String, sobreNome: String) {
+        if (resposta > "18") {
+            txv_Resposta.text = "${nome} ${sobreNome} a sua idade é : ${resposta} anos! Já pode dirigir!!"
+            txv_Resposta.setTextColor(Color.GREEN)
+        } else if (resposta < "18") {
+            txv_Resposta.text = "${nome} ${sobreNome} a sua idade é : ${resposta} anos! Você ainda é menor de idade!"
             txv_Resposta.setTextColor(Color.BLUE)
         }
     }
 
-    private fun validarCampos(nome: String, sobreNome : String , ano : String) {
+    private fun  validarCampos(nome: String, sobreNome : String , ano : String):Boolean {
         if (nome.length < 3) {
             Toast.makeText(this, "O nome deve ser maior que 3 digitos", Toast.LENGTH_SHORT).show()
+            return false
         }
         if(nome.equals("")){
             Toast.makeText(this, "O nome nao pode ser vazio", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (sobreNome.length < 3) {
+            Toast.makeText(this, "O sobrenome deve ser maior que 3 digitos", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if(sobreNome.equals("")){
+            Toast.makeText(this, "O sobrenome nao pode ser vazio", Toast.LENGTH_SHORT).show()
+            return false
         }
         if(ano.equals("") ){
-            Toast.makeText(this, "O nome nao pode ser vazio", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "O ano nao pode ser vazio", Toast.LENGTH_SHORT).show()
+            return false
         }
+        if(ano.length != 4 ){
+            Toast.makeText(this, "O ano deve conter 4 digitos", Toast.LENGTH_SHORT).show()
+            return false
+        }
+       return true
     }
 }
